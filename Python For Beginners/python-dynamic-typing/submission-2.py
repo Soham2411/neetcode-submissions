f0760c88314cef10e4b60dd841cb380a76fae219
Soham Bose <47531051+Soham2411@users.noreply.github.com>
2026-08-22 21:05:35 -0400

    Add: python-dynamic-typing - submission-2

---
+++ Python For Beginners/python-dynamic-typing/submission-2.py
@@ -0,0 +1,14 @@
+variable=10
+print(type(variable))
+
+variable=1.0
+print(type(variable))
+
+variable = True
+print(type(variable))
+
+variable = "Hello"
+print(type(variable))
+
+variable = []
+print(type(variable))
